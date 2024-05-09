71722fe472b5ff62efe79c550a589e45ba79d05d
EmVegito <ay2728850@gmail.com>
2024-05-09 13:01:16 +0530

    shad cn sheet

---
+++ components/MobileNavebar.tsx
@@ -0,0 +1,13 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+
+const MobileNavebar = ({ user }: MobileNavProps) => {
+  return <div>MobileNavebar</div>
+}
+export default MobileNavebar
